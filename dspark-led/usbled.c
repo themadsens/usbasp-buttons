ab74057fd3335255fa04405d600eb63e765c1977
Flemming Madsen <flemming@themadsens.dk>
2018-12-26 16:28:31 +0100

    Add digispark blinker

--- dspark-led/usbled.c
+++ dspark-led/usbled.c
@@ -23,26 +23,15 @@
 #define USBCMD_SCROLL   3
 #define USBCMD_FETCHSTR 4
 
-#define LEDG_PIN    SBIT(PORTC, PC1)
-#define LEDG_OUT    SBIT(DDRC,  PC1)
-#define LEDR_PIN    SBIT(PORTC, PC0)
-#define LEDR_OUT    SBIT(DDRC,  PC0)
+#define LED_PIN    SBIT(PORTB, PB1)
+#define LED_OUT    SBIT(DDRB,  PB1)
 
-#define LED0_PIN    SBIT(PORTC, PC2)
-#define LED0_OUT    SBIT(DDRC,  PC2)
-#define LED1_PIN    SBIT(PORTB, PB2)
-#define LED1_OUT    SBIT(DDRB,  PB2)
-#define LED2_PIN    SBIT(PORTB, PB4)
-#define LED2_OUT    SBIT(DDRB,  PB4)
-#define LED3_PIN    SBIT(PORTB, PB5)
-#define LED3_OUT    SBIT(DDRB,  PB5)
-
-#define BTN1_PIN SBIT(PINB,  PB3)
-#define BTN1_PU  SBIT(PORTB, PB3)
-#define BTN2_PIN SBIT(PIND,  PD1)
-#define BTN2_PU  SBIT(PORTD, PD1)
-#define BTN3_PIN SBIT(PIND,  PD0)
-#define BTN3_PU  SBIT(PORTD, PD0)
+#define LED1_PIN    SBIT(PORTB, PB0)
+#define LED1_OUT    SBIT(DDRB,  PB0)
+#define LED2_PIN    SBIT(PORTB, PB1)
+#define LED2_OUT    SBIT(DDRB,  PB1)
+#define LED3_PIN    SBIT(PORTB, PB2)
+#define LED3_OUT    SBIT(DDRB,  PB2)
 
 #define TIMERRATE     10000
 #define TIMERTOP  F_CPU / TIMERRATE / 8
@@ -60,8 +49,7 @@ int usbIntr (char *s, u8 btn, u8 chg)
 char str[90];
 #define SPRINT(fmt, ...) (sprintf_P(str, PSTR(fmt "\r\n"), ##__VA_ARGS__), str)
 
-u8 ledR = 1;
-u8 ledG = 0;
+u8 led0 = 1;
 u8 led1 = 0;
 u8 led2 = 0;
 u8 led3 = 0;
@@ -71,8 +59,7 @@ ISR(TIMER0_OVF_vect)
 {
 	deciMilli++;
     TCNT0 = 255 - TIMERTOP;
-    LEDR_PIN = deciMilli < 2 && ledR ? 0 : 1;
-    LEDG_PIN = deciMilli < 2 && ledG ? 0 : 1;
+    LED_PIN  = deciMilli < 9 && led0 ? 1 : 0;
     LED1_PIN = deciMilli < 2 && led1 ? 1 : 0;
     LED2_PIN = deciMilli < 2 && led2 ? 1 : 0;
     LED3_PIN = deciMilli < 2 && led3 ? 1 : 0;
@@ -82,31 +69,23 @@ extern void setup(void)
 {
   milliSecs = 0;
 
-  TCCR0 = BIT(CS01);    // Timer0 /8 clock prescaler
+  TCCR0B = BIT(CS01);    // Timer0 /8 clock prescaler
   SBIT(TIMSK, TOIE0) = 1;
-  LEDG_OUT = 1;
-  LEDG_PIN = 1;
-  LEDR_OUT = 1;
-  LEDR_PIN = 1;
-
-  BTN1_PU = 1;
-  BTN2_PU = 1;
-  BTN3_PU = 1;
+  LED_OUT = 1;
+  LED_PIN = 1;
 }
 
-struct { u16 p1, p2; } ledCnt[4];
+struct { u16 p1, p2; } ledCnt[3];
 int initialised = 0;
 static void initPins()
 {
   memset(ledCnt, 0, sizeof(ledCnt));
-  ledR = 0;
+  led0 = 0;
 
-  LED0_OUT = 1;
   LED1_OUT = 1;
   LED2_OUT = 1;
   LED3_OUT = 1;
 
-  LED0_PIN = 0;
   LED1_PIN = 0;
   LED2_PIN = 0;
   LED3_PIN = 0;
@@ -122,9 +101,9 @@ void handleLed()
 
   int m = 0, i;
   if (scrollMode)
-    m = 1 << (milliSecs % 1000 / 250);
+    m = 1 << (milliSecs % 750 / 250);
   else {
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 3; i++) {
       if (ledCnt[i].p1 == 1)
         m |= 1 << i;
       else if (ledCnt[i].p1 == 0)
@@ -135,52 +114,11 @@ void handleLed()
         m |= 1 << i;
     }
   }
-  LED0_PIN = m&1 ? 1:0;
   led1 = m&2 ? 1:0;
   led2 = m&4 ? 1:0;
   led3 = m&8 ? 1:0;
 }
 
-
-// Handle buttons
-i16 btnTime = 0;
-u16 btnMask = 0;
-u16 btnPrev = 0;
-
-void handleButtons()
-{
-#if 0
-  if (!initialised)
-    return;
-#endif
-
-  int i, mask;
-  for (i = 0, mask=0; i < 3; i++) {
-    int b;
-    switch (i) {
-     case 0: b = BTN1_PIN==0; break;
-     case 1: b = BTN2_PIN==0; break;
-     case 2: b = BTN3_PIN==0; break;
-    }
-    mask |= (b?1:0) << i;
-  }
-  if (mask != btnMask) {
-    btnMask = mask;
-    btnTime = milliSecs & 0xffff;
-  }
-  else if ((i16)(milliSecs & 0xffff) - btnTime > 50 && btnMask != btnPrev) {
-    int btnChng = btnMask ^ btnPrev;
-    usbIntr(SPRINT("BTN %d,%d,%d CHG %d,%d,%d", btnMask&1, (btnMask>>1)&1, (btnMask>>2)&1,
-                                                btnChng&1, (btnChng>>1)&1, (btnChng>>2)&1), btnMask, btnChng);
-    btnPrev = btnMask;
-    ledR = btnMask != 0;
-    if (!initialised) {
-      initPins();
-      ledCnt[0].p1 = 50;
-      ledCnt[0].p2 = 1950;
-    }
-  }
-}
 
 extern void repeat(void)
 {
@@ -193,16 +131,11 @@ extern void repeat(void)
     return;
   }
 
-  if (0 == (milliSecs % 500)) {
-    ledG = ~ledG;
-  }
-
-  handleButtons();
   handleLed();
 
 
   if (0 == (milliSecs % 5000)) {
-    usbIntr(SPRINT("HELLO %10lu %lu %hu %d/%d %d,%d,%d", milliSecs, btnMask, deciMilli, msIn,
+    usbIntr(SPRINT("HELLO %10lu %d/%d %d,%d,%d", milliSecs, deciMilli, msIn,
                                                         ledCnt[0].p1, ledCnt[0].p2, (milliSecs % (ledCnt[0].p1 * 2))), 0, 0);
   }
 }
